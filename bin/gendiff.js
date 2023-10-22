829c0ab2e60864d4c075d2898ce1afe4c7c3fe0c
Mariya Belinskaya <mv.belinskaya@gmail.com>
2023-10-22 22:05:31 +0500

    update bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,16 +1,24 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
+import getParse from './parsers.js';
+import fs from 'fs';
+import path from 'path';
 
 program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0');
+    .version('1.0.0')
+    .option('-f, --format <type>', 'output format')
+    .arguments('<filepath1> <filepath2>')
+    .action((filepath1, filepath2) => {
 
-// program.command('join')
-//   .description('Команда соединяет две строки в одну')
-//   .argument('<first>', 'первая строка')
-//   .argument('<second>', 'вторая строка')
-//   .option('-c, --connector <type>', 'соединительная строка', '')
+    const fullPath1 = fs.readFileSync(path.resolve(process.cwd(), filepath1));
+    const fullPath2 = fs.readFileSync(path.resolve(process.cwd(), filepath2));
+
+    const result1 = getParse(fullPath1);
+    const result2 = getParse(fullPath2);
+        return console.log(result1, result2);
+    })
 
 program.parse();
