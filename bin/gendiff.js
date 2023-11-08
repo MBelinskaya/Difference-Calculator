46b1a99efffd10ea8f9ff28249d9eb2e98ba1005
Mariya Belinskaya <mv.belinskaya@gmail.com>
2023-11-08 14:31:36 +0500

    update bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,36 +1,27 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
-import getParsedData from './parse.js';
 import fs from 'fs';
 import path from 'path';
-import getDiff from './getDiff.js';
+// eslint-disable-next-line import/extensions
+import getType from '../src/getType.js';
+// eslint-disable-next-line import/extensions
+import makeStylish from '../src/formatting.js';
 
 program
-    .name('gendiff')
-    .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0')
-    .option('-f, --format <type>', 'output format')
-    .arguments('<filepath1> <filepath2>')
-    .action((filepath1, filepath2) => {
-
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
     const fullPath1 = fs.readFileSync(path.resolve(process.cwd(), filepath1));
     const fullPath2 = fs.readFileSync(path.resolve(process.cwd(), filepath2));
+    const parsedData1 = JSON.parse(fullPath1);
+    const parsedData2 = JSON.parse(fullPath2);
+    const diffResult = getType(parsedData1, parsedData2);
 
-    const parsedData1 = getParsedData(fullPath1);
-    const parsedData2 = getParsedData(fullPath2);
-
-    const diffResult = getDiff(parsedData1, parsedData2);
-
-    const result = JSON.stringify(diffResult, (key, value) => {
-        if (value === "deleted") return '-' + key + value
-    
-        return value;
-    }, 2);
-
-// 
-    return console.log(result);
-
-    })
+    return console.log((makeStylish(diffResult)));
+  });
 
 program.parse();
