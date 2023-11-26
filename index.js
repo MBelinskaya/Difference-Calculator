aece77907106d3101188d52ed7deb5822f019aa9
Mariya Belinskaya <mv.belinskaya@gmail.com>
2023-11-26 15:42:42 +0500

    add index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+import getType from './src/getType.js';
+import makeStylish from './formatters/stylish.js';
+import getParsedData from './src/parser.js';
+
+const readFile = (filepath) => {
+  const file = fs.readFileSync(path.resolve(process.cwd(), filepath));
+  const extension = path.extname(filepath).slice(1);
+  return getParsedData(file, extension);
+};
+
+const getGenDiff = (file1, file2, format = 'stylish') => {
+  const newTree = getType(readFile(file1), readFile(file2));
+  return makeStylish(newTree, format);
+};
+
+export default getGenDiff;
