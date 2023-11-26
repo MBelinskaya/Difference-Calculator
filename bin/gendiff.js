6e0f732b54f675b8df08af74c369c1298b2974af
Mariya Belinskaya <mv.belinskaya@gmail.com>
2023-11-26 15:37:51 +0500

    update bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,27 +1,16 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
-import fs from 'fs';
-import path from 'path';
-// eslint-disable-next-line import/extensions
-import getType from '../src/getType.js';
-// eslint-disable-next-line import/extensions
-import makeStylish from '../src/formatting.js';
+import getGenDiff from '../index.js';
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-    const fullPath1 = fs.readFileSync(path.resolve(process.cwd(), filepath1));
-    const fullPath2 = fs.readFileSync(path.resolve(process.cwd(), filepath2));
-    const parsedData1 = JSON.parse(fullPath1);
-    const parsedData2 = JSON.parse(fullPath2);
-    const diffResult = getType(parsedData1, parsedData2);
-
-    return console.log((makeStylish(diffResult)));
+    console.log(getGenDiff(filepath1, filepath2, program.opts().format));
   });
 
 program.parse();
