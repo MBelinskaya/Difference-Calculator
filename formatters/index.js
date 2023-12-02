50e4122d3c5d6b8f445ede8c1ad05c1f7bcb25a3
Mariya Belinskaya <mv.belinskaya@gmail.com>
2023-12-02 19:43:43 +0500

    added format selection in formatters/index.js

--- formatters/index.js
+++ formatters/index.js
@@ -1,1 +1,12 @@
-// Реализуйте возможность выбора формата plain.
+import makeStylish from "./stylish.js";
+import makePlain from "./plain.js";
+
+const formatSelection = (tree, format) => {
+switch (format) {
+    case 'stylish':
+        return makeStylish(tree);
+    case 'plain':
+        return makePlain(tree);
+}
+};
+export default formatSelection;
