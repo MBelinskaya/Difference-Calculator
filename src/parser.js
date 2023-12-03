df88fb95d911448964a73466b789639140b5f919
Mariya Belinskaya <mv.belinskaya@gmail.com>
2023-12-03 15:06:05 +0500

    update src/parser.js

--- src/parser.js
+++ src/parser.js
@@ -1,14 +1,11 @@
 import yaml from 'js-yaml';
 
 const getParsedData = (data, ext) => {
-  // console.log(typeof ext, `!!!${ext}!!`);
   switch (ext) {
     case 'json':
       return JSON.parse(data);
     case 'yml':
       return yaml.load(data);
-    case 'stylish':
-      return JSON.stringify(data, null, ' ');
     default:
       throw new Error(`"${ext}" unknown extension`);
   }
