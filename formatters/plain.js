144cc1758a4fbf2fd626eda7feffddafb3cb5f7b
Mariya Belinskaya <mv.belinskaya@gmail.com>
2023-12-03 15:05:04 +0500

    update output in the plain format

--- formatters/plain.js
+++ formatters/plain.js
@@ -1,41 +1,40 @@
 import _ from 'lodash';
 
 const stringify = (currentValue) => {
-    if (_.isObject(currentValue)) {
-    return `[complex value]`;
-    }
-    if (_.isString(currentValue)) {
-        return `'${currentValue}'`;
-    }
-    return `${currentValue}`;
+  if (_.isObject(currentValue)) {
+    return '[complex value]';
+  }
+  if (_.isString(currentValue)) {
+    return `'${currentValue}'`;
+  }
+  return `${currentValue}`;
 };
 
 const getPath = (paths) => paths.flat().join('.');
 
 const makePlain = (treeNew) => {
-    const iter = (tree, currentPath) => {
+  const iter = (tree, currentPath) => {
     const result = tree.map((data) => {
-        const generatePath = getPath([currentPath, data.name]);
-        switch (data.type) {
-    case 'nested':
-            return iter(data.value, generatePath);
+      const generatePath = getPath([currentPath, data.name]);
+      switch (data.type) {
+        case 'nested':
+          return iter(data.value, generatePath);
         case 'deleted':
-            return `Property '${generatePath.slice(1)}' was removed`;
-    case 'added':
-        return `Property '${generatePath.slice(1)}' was added with value: ${stringify(data.value)}`;
+          return `Property '${generatePath.slice(1)}' was removed`;
+        case 'added':
+          return `Property '${generatePath.slice(1)}' was added with value: ${stringify(data.value)}`;
         case 'changed':
-            return `Property '${generatePath.slice(1)}' was updated. From ${stringify(data.value1)} to ${stringify(data.value2)}`;
+          return `Property '${generatePath.slice(1)}' was updated. From ${stringify(data.value1)} to ${stringify(data.value2)}`;
         case 'unchanged':
-            return null;
+          return null;
         default: {
-            throw new Error(`"${data.type}" unknown type`); }
-        }
+          throw new Error(`"${data.type}" unknown type`); }
+      }
     }).join('\n');
 
     return result;
-    };
-    return iter(treeNew, '');
+  };
+  return iter(treeNew, '');
 };
 
 export default makePlain;
-
