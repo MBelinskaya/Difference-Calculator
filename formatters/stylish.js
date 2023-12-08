5de8dc43c58e1669e64931b182adea7bd7b89bc0
Mariya Belinskaya <mv.belinskaya@gmail.com>
2023-12-08 19:04:27 +0500

    update output in the stylish format

--- formatters/stylish.js
+++ formatters/stylish.js
@@ -1,12 +1,11 @@
 import _ from 'lodash';
 
 const indent = ' ';
-// const indent = '.';
 const spacesCount = 4;
 
-const currentIndent = (depth) => indent.repeat(depth * spacesCount - 2);
+const beforeIndent = (depth) => indent.repeat(depth * spacesCount - 2);
 const bracketIndent = (depth) => indent.repeat(depth * spacesCount - 4);
-const withoutIndent = (depth) => indent.repeat(depth * spacesCount);
+const currentIndent = (depth) => indent.repeat(depth * spacesCount);
 
 const stringify = (currentValue, depth) => {
   if (!_.isObject(currentValue)) {
@@ -14,7 +13,7 @@ const stringify = (currentValue, depth) => {
   }
   const lines = Object
     .entries(currentValue)
-    .map(([key, val]) => `${withoutIndent(depth)}${key}: ${stringify(val, depth + 1)}`);
+    .map(([key, val]) => `${currentIndent(depth)}${key}: ${stringify(val, depth + 1)}`);
   return [
     '{',
     ...lines,
@@ -25,28 +24,30 @@ const stringify = (currentValue, depth) => {
 const makeStylish = (treeNew) => {
   const iter = (tree, depth) => {
     const result = tree.map((data) => {
-    // console.log(`!!!${currentIndent(depth)}${depth} ${data.type} ${data.name}`);
-
       switch (data.type) {
         case 'nested':
-          return `${currentIndent(depth)}  ${data.name}: ${stringify(iter(data.value, depth + 1), depth)}`;
+          return `${beforeIndent(depth)} ${data.name}: ${stringify(iter(data.value, depth + 1), depth)}`;
         case 'deleted':
-          return `${currentIndent(depth)}- ${data.name}: ${stringify(data.value, depth + 1)}`;
+          return `${beforeIndent(depth)}- ${data.name}: ${stringify(data.value, depth + 1)}`;
         case 'added':
-          return `${currentIndent(depth)}+ ${data.name}: ${stringify(data.value, depth + 1)}`;
+          return `${beforeIndent(depth)}+ ${data.name}: ${stringify(data.value, depth + 1)}`;
         case 'changed':
           return [
-            `${currentIndent(depth)}- ${data.name}: ${stringify(data.value1, depth + 1)}`,
-            `${currentIndent(depth)}+ ${data.name}: ${stringify(data.value2, depth + 1)}`,
+            `${beforeIndent(depth)}- ${data.name}: ${stringify(data.value1, depth + 1)}`,
+            `${beforeIndent(depth)}+ ${data.name}: ${stringify(data.value2, depth + 1)}`,
           ].join('\n');
         case 'unchanged':
-          return `${currentIndent(depth)}  ${data.name}: ${stringify(data.value, depth + 1)}`;
+          return `${beforeIndent(depth)}  ${data.name}: ${stringify(data.value, depth + 1)}`;
         default: {
           throw new Error(`"${data.type}" unknown extension`); }
       }
-    }).join('\n');
+    });
 
-    return `{\n${result}\n}`;
+    return [
+      '{',
+      ...result,
+      `${bracketIndent(depth)}}`,
+    ].join('\n');
   };
   return iter(treeNew, 1);
 };
