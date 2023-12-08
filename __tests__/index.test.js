38f5f62de919d506bdd6f0013044b183a9e77e21
Mariya Belinskaya <mv.belinskaya@gmail.com>
2023-12-08 19:08:34 +0500

    update index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,16 +1,13 @@
 import fs from 'fs';
 import { test, expect } from '@jest/globals';
-import path from 'path';
+import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 import getGenDiff from '../index';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '.', '__fixtures__', filename);
 
-
 test('gendiff', () => {
   expect(getGenDiff('filepath1.json', 'filepath2.json')).toEqual(fs.readFileSync(getFixturePath('expected_JSON.txt'), 'utf-8'));
-  expect(getGenDiff('file1.json', 'file2.json')).toEqual(fs.readFileSync(getFixturePath('expected_flat_JSON.txt'), 'utf-8'));
 });
