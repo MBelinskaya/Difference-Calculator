3e4c5c62549a8a4d322c0f405f391670aff806ad
Mariya Belinskaya <mv.belinskaya@gmail.com>
2023-12-09 16:33:37 +0500

    update index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,5 +9,7 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '.', '__fixtures__', filename);
 
 test('gendiff', () => {
+  expect(getGenDiff('file1.json', 'file2.json')).toBe(fs.readFileSync(getFixturePath('expected_flat_JSON.txt'), 'utf-8'));
   expect(getGenDiff('filepath1.json', 'filepath2.json')).toEqual(fs.readFileSync(getFixturePath('expected_JSON.txt'), 'utf-8'));
+  expect(getGenDiff('filepath1.json', 'filepath2.json', 'plain')).toBe(fs.readFileSync(getFixturePath('plain_json.txt'), 'utf-8'));
 });
