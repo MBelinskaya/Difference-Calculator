e0d5f7929a033b4b5b6bc2d8d5d2d294921fe3a8
Mariya Belinskaya <mv.belinskaya@gmail.com>
2023-12-17 17:01:45 +0500

    update generteDifference.js

---
+++ src/generteDifference.js
@@ -0,0 +1,32 @@
+import _ from 'lodash';
+
+const generteDifference = (data1, data2) => {
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+  const allKeys = _.sortBy(_.union(keys1, keys2));
+
+  const result = allKeys.map((node) => {
+    if (_.isObject(data1[node]) && _.isObject(data2[node])) {
+      return { name: node, type: 'nested', value: generteDifference(data1[node], data2[node]) };
+    }
+    if (!Object.hasOwn(data1, node)) {
+      return { name: node, type: 'added', value: data2[node] };
+    }
+    if (!Object.hasOwn(data2, node)) {
+      return { name: node, type: 'deleted', value: data1[node] };
+    }
+    if (_.isEqual(data1[node], data2[node])) {
+      return { name: node, type: 'unchanged', value: data1[node] };
+    }
+    if (!_.isEqual(data1[node], data2[node])) {
+      return {
+        name: node, type: 'changed', value1: data1[node], value2: data2[node],
+      };
+    }
+    return result;
+  });
+
+  return result;
+};
+
+export default generteDifference;
