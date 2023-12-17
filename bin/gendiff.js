e0982c0310b0d6d3396b7066872d2bb4f9d4660c
Mariya Belinskaya <mv.belinskaya@gmail.com>
2023-12-17 16:57:00 +0500

    update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
-import getGenDiff from '../index.js';
+import genDiff from '../src/index.js';
 
 program
   .name('gendiff')
@@ -10,7 +10,7 @@ program
   .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-    console.log(getGenDiff(filepath1, filepath2, program.opts().format));
+    console.log(genDiff(filepath1, filepath2, program.opts().format));
   });
 
 program.parse();
